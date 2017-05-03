5325c53133247b1c11ed4d599a02b3807b2ad841
Valentino Ekaputra <silentrazgriz@gmail.com>
2017-05-03 19:15:52 +0700

    forget query.sql

--- docs/query.sql
+++ docs/query.sql
@@ -2,6 +2,4 @@ UPDATE `event_answers` SET `answer` = REPLACE(answer, '"provider":"3"', '"provid
 UPDATE `event_answers` SET `answer` = REPLACE(answer, '"number"', '"new_number"') WHERE `answer` not like '%new_number%';
 UPDATE `event_answers` SET `answer` = REPLACE(answer, '"package"', '"old_number":"","package"') WHERE `answer` not like '%old_number%';
 
-UPDATE `event_answers` SET `answer` = REPLACE(REPLACE(answer, '"moment":{"twitter"', '"twitter"'), '},"hadiah_voucher"', ',"hadiah_voucher"');
-
 UPDATE `events` SET `kpi` = REPLACE(kpi, 'reportUnit', 'report_unit');
